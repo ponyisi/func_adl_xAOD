020554c8920d2768266526f4536281d2c27f054b
ponyisi <ponyisi@utexas.edu>
2021-02-08 12:51:58 -0600

    TruthParticle::parent/child methods have pointer return values

--- func_adl_xAOD/xAODlib/EventCollections.py
+++ func_adl_xAOD/xAODlib/EventCollections.py
@@ -125,3 +125,4 @@ ctyp.add_method_type_info("xAOD::TruthParticle", "prodVtx", ctyp.terminal('xAODT
 ctyp.add_method_type_info("xAOD::TruthParticle", "decayVtx", ctyp.terminal('xAODTruth::TruthVertex', is_pointer=True))
 ctyp.add_method_type_info("xAOD::TruthParticle", "parent", ctyp.terminal('xAOD::TruthParticle', is_pointer=True))
 ctyp.add_method_type_info("xAOD::TruthParticle", "child", ctyp.terminal('xAOD::TruthParticle', is_pointer=True))
+                         
